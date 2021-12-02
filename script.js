e5c54a9dc44d397c20b6d7f28011af6e2e200248
JideOgun <JideOgun@github.com>
2021-12-02 11:59:45 -0600

    updates

--- script.js
+++ script.js
@@ -6,24 +6,36 @@ const m = moment().format('LLL');
  rowEl = $('.row')
 
 
- var createTimeBlockRow = function() {
+ var createTimeBlockRow = function(hourId) {
      let timeblock = $("<div>").addClass('row');
-     $(timeblock).attr('id', 'timeblock-9');
+     $(timeblock).attr('id', `timeblock-${hourId}`);
      return timeblock
  }
- containerEl.append(createTimeBlockRow);
 
- function createCol(element, colSize){
-     const col = document.createElement('div');
-     col.classList.add(`col-${colSize}`, 'p-0');
-     col.appendChild(element);
-     return col;
+
+ for( var i = 9; i <= 18; i++) {
+     const timeblock = createTimeBlockRow(i);
+     columns = createCol();
+     containerEl.append(timeblock);
+        
+        createCol();
+        timeblock.append(createCol())
  }
 
- var createHourDiv = function(hour) {
+
+
+ function createCol(){
+     for( var i = 0; i < 3; i++){
+    let col = $('<div>').addClass(`col-`, 'p-0');
+    return col; }
+    
+}
+
+
+ var createHourDiv = function() {
     let hourdiv = $("<div>").addClass('hourdiv');
     $(hourdiv).attr('id', 'nineam-div');
-    $(hourdiv).text = formatHour (hour)
+    $(hourdiv).text = formatHour ()
     return hourdiv;
 }
 
@@ -33,11 +45,16 @@ var createTextArea = function() {
     return textDiv
 }
 
-var saveBtn = function() {
+var createSaveBtn = function() {
     const savebtn = $("button").addClass('button');
     $(savebtn).attr('id', 'button-id')
-
 }
 
+function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) {
+    const innerCols = [hourCol, textAreaCol, saveBtnCol];
+    for (let col of innerCols) {
+      timeblockRow.appendChild(col);
+    }
+  }
 
 
