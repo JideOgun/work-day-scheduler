f39af587897a143a3d7371460f53d5ac758ac78f
JideOgun <JideOgun@github.com>
2021-12-03 10:48:34 -0600

    updates

--- script.js
+++ script.js
@@ -1,63 +1,30 @@
 let currentDayEl = document.getElementById("currentDay")
-const m = moment().format('LLL');
- currentDayEl.textContent = m;
+const time = moment().format('LLL');
 
- containerEl = $('.container');
- rowEl = $('.row')
+// Add the current date to the screen
+ currentDayEl.textContent = time;
+ let currentHour = moment().format('LT');
+ console.log(currentHour);
+ 
 
 
- var createTimeBlockRow = function(hourId) {
-     let timeblock = $("<div>").addClass('row');
-     $(timeblock).attr('id', `timeblock-${hourId}`);
-     return timeblock
+ //grab each hour 
+ $(".hour").each(function(){
+     var timediv = $(this).text();
+     let timediv1 = moment(timediv)
+     console.log(timediv1);
+    
+    if(currentHour > timediv1 ){
+    console.log('it worked and current time is greater than timeblock time');
+     $('.timeblock').addClass('past')
  }
-
-
- for( var i = 9; i <= 18; i++) {
-     const timeblock = createTimeBlockRow(i);
-     columns = createCol();
-     containerEl.append(timeblock);
-        
-        createCol();
-        timeblock.append(createCol())
- }
-
-
-
- function createCol(){
-     let collection = []
-     for( var i = 0; i < 3; i++){
-    let col = $('<div>').addClass(`col-${i + 1}`, 'p-0');
-    collection.push(col)
-    col.textContent = "dfghjk"
- }
- return collection;
+     else if (currentHour === timediv1){
+ $('.timeblock').addClass('present');
+ console.log('it worked and current time is equal to timeblock time');
 }
-
-
- var createHourDiv = function() {
-    let hourdiv = $("<div>").addClass('hourdiv');
-    $(hourdiv).attr('id', 'nineam-div');
-    $(hourdiv).text = formatHour ()
-    return hourdiv;
-}
-
-var createTextArea = function() {
-    let textDiv = $("<div>").addClass('textarea-div');
-    $(textDiv).attr('id', 'textarea-id')
-    return textDiv
-}
-
-var createSaveBtn = function() {
-    const savebtn = $("button").addClass('button');
-    $(savebtn).attr('id', 'button-id')
-}
-
-function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) {
-    const innerCols = [hourCol, textAreaCol, saveBtnCol];
-    for (let col of innerCols) {
-      timeblockRow.appendChild(col);
-    }
-  }
-
-
+    else if (currentHour < timediv1){
+    $('.timeblock').addClass('future');
+    console.log('it worked and current time is less than timeblock time');
+                }
+ })
+ 
