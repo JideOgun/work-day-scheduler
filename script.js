8f1c04aea7cb5738cb2462545323dcc869adc457
JideOgun <JideOgun@github.com>
2021-12-03 14:10:28 -0600

    updates

--- script.js
+++ script.js
@@ -31,29 +31,47 @@ const time = moment().format('LLL');
    } 
     
 
+
+
   // store items to local storage
 $('.saveBtn').click(function() {
-   
 
-    var value =  $.trim($('.description').val());
-    console.log(value);
+    var value9 =  $.trim($('#ta9').val());
+    var value10 =  $.trim($('#ta10').val());
+    var value11 =  $.trim($('#ta11').val());
+    var value12 =  $.trim($('#ta12').val());
+    var value13 =  $.trim($('#ta13').val());
+    var value14 =  $.trim($('#ta14').val());
+    var value15 =  $.trim($('#ta15').val());
+    var value16 =  $.trim($('#ta16').val());
+    var value17 =  $.trim($('#ta17').val());
+    console.log(value17);
     
-localStorage.setItem('.description', value);
+localStorage.setItem('description9', value9);
+localStorage.setItem('description10', value10);
+localStorage.setItem('description11', value11);
+localStorage.setItem('description12', value12);
+localStorage.setItem('description13', value13);
+localStorage.setItem('description14', value14);
+localStorage.setItem('description15', value15);
+localStorage.setItem('description16', value16);
+localStorage.setItem('description17', value17);
 })
 
+$("#ta9").val(localStorage.getItem('description9'));
+$("#ta10").val(localStorage.getItem('description10'))
+$("#ta11").val(localStorage.getItem('description11'))
+$("#ta12").val(localStorage.getItem('description12'))
+$("#ta13").val(localStorage.getItem('description13'))
+$("#ta14").val(localStorage.getItem('description14'))
+$("#ta15").val(localStorage.getItem('description15'))
+$("#ta16").val(localStorage.getItem('description16'))
+$("#ta17").val(localStorage.getItem('description17'))
 
-// // retrieve values
- 
-//   $("#hour-9 .time-block").val(localStorage.getItem("9"));
-//   $("#hour-10 .time-block").val(localStorage.getItem("10"));
-//   $("#hour-11 .time-block").val(localStorage.getItem("11"));
-//   $("#hour-12 .time-block").val(localStorage.getItem("12"));
-//   $("#hour-13 .time-block").val(localStorage.getItem("13"));
-//   $("#hour-14 .time-block").val(localStorage.getItem("14"));
-//   $("#hour-15 .time-block").val(localStorage.getItem("15"));
-//   $("#hour-16 .time-block").val(localStorage.getItem("16"));
-//   $("#hour-17 .time-block").val(localStorage.getItem("17"));
-// })
+$('.clearfields').click(function(){
+    $('#ta9').val() = '';
+    localStorage.clear()
+})
 
 
 
